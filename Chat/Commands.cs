4a7324f3ed7b60967e9125a219abcd30e6c1421a
Nikro <nikromw@mail.ru>
2021-09-05 16:35:30 +0300

    feature : created main entities

---
+++ Chat/Commands.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Server
+{
+    public static class Commands
+    {
+        public const string LogIn = "Login";
+        public const string Regestration = "Regestration";
+        public const string Message = "Message";
+
+    }
+}
---
+++ Chat/Server.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace Server
+{
+    public class Server
+    {
+        static TcpListener tcpListener;
+        List<User> clients = new List<User>();
+
+        protected internal void AddConnection(User clientObject)
+        {
+            clients.Add(clientObject);
+        }
+        protected internal void RemoveConnection(string id)
+        {
+
+            User client = clients.FirstOrDefault(c => c.Id == id);
+
+            if (client != null)
+                clients.Remove(client);
+        }
+
+        protected internal void Listen()
+        {
+            try
+            {
+                tcpListener = new TcpListener(IPAddress.Any, 8888);
+                tcpListener.Start();
+                Console.WriteLine("Сервер запущен. Ожидание подключений...");
+
+                while (true)
+                {
+                    TcpClient tcpClient = tcpListener.AcceptTcpClient();
+
+                    User clientObject = new User(tcpClient, this);
+                    Thread clientThread = new Thread(new ThreadStart(clientObject.Process));
+                    clientThread.Start();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Disconnect();
+            }
+        }
+
+   
+        protected internal void BroadcastMessage(string message, string id)
+        {
+            byte[] data = Encoding.Unicode.GetBytes(message);
+            for (int i = 0; i < clients.Count; i++)
+            {
+                if (clients[i].Id != id) 
+                {
+                    clients[i].Stream.Write(data, 0, data.Length); 
+                }
+            }
+        }
+
+        protected internal void Disconnect()
+        {
+            tcpListener.Stop();
+
+            for (int i = 0; i < clients.Count; i++)
+            {
+                clients[i].Close();
+            }
+            Environment.Exit(0);
+        }
+    }
+}
